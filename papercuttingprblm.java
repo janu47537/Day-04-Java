5d466c5ac6f1482acf450e85b1bbfce0791e04be
janu47537 <janu47537@gmail.com>
2024-12-05 15:35:36 +0530

    Add files via upload

---
+++ papercuttingprblm.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class papercuttingprblm
+{
+	public static void main (String[] args)
+	{
+	Scanner s=new Scanner(System.in);
+	int t=s.nextInt();
+	for(int i=0;i<t;i++)
+	{
+	    int a=s.nextInt();
+	    int b=s.nextInt();
+	    int c=(a/b)*(a/b);
+	
+	System.out.println(c);
+
+	}
+}
+}
