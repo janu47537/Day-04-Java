0e16c8a2110e6cae8a84e1c7752caed33fd432f8
janu47537 <janu47537@gmail.com>
2024-12-05 12:22:08 +0530

    Add files via upload

---
+++ forloopeven.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class forloopeven{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<=10;i+=2)
+        {
+            System.out.println(i);
+            
+        }
+    }
+}
