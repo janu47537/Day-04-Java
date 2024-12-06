fa9eb91e8a455243dd3304754abab05688e45c17
janu47537 <janu47537@gmail.com>
2024-12-06 09:51:54 +0530

    Add files via upload

---
+++ printnnaturalnum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class printnnaturalnum{
+    public static void main(String[]args){
+        int n=10;
+        int i=n-1;
+        while(i>=0)
+        {
+            System.out.println(n-i);
+            i-=1;   
+    }
+}
+}
